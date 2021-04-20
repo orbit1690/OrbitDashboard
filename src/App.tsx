4bca8bb1e1251cc6dd5f13587a708ecdf231b734
NoamPrag <57250657+NoamPrag@users.noreply.github.com>
2021-04-20 08:58:36 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const App = (): JSX.Element => <div>Hello, World!</div>;
+
+export default App;
---
+++ src/renderer.tsx
@@ -0,0 +1,8 @@
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const ROOT: HTMLElement = document.getElementById("root");
+
+ReactDOM.render(<App />, ROOT);
