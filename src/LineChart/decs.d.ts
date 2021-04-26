3c752e75a7584bf785532bb7100d5a35f16c96bc
Noam Prag <noam.prag0@gmail.com>
2021-04-27 01:59:28 +0300

    Added a comment explaining decs.d.ts on LineChart

--- src/LineChart/decs.d.ts
+++ src/LineChart/decs.d.ts
@@ -1,1 +1,3 @@
+// Because CanvasJS-react-charts is an old library which uses old JavaScript, it doesn't have its TypeScript types.
+// In order to use this library in TypeScript, we need to declare it as a module in a declaration file (.d.ts).
 declare module "canvasjs-react-charts";
