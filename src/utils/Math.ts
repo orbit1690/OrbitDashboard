f18c34cc1097b87609f80ae5174e8b1e2bfb2c37
Noam Prag <noam.prag0@gmail.com>
2021-05-05 20:37:21 +0300

    Exported angle convertion functions

--- src/utils/Math.ts
+++ src/utils/Math.ts
@@ -1,4 +1,4 @@
 const radiansToDegrees: number = 180 / Math.PI;
 const degreesToRadians: number = Math.PI / 180;
-const degrees = (radians: number): number => radians * radiansToDegrees;
-const radians = (degrees: number): number => degrees * degreesToRadians;
+export const degrees = (radians: number): number => radians * radiansToDegrees;
+export const radians = (degrees: number): number => degrees * degreesToRadians;
