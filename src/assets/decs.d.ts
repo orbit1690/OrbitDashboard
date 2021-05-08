9a9ec55a7f886adff0d37d94942b55092ad9bb7a
Noam Prag <noam.prag0@gmail.com>
2021-05-08 19:22:32 +0300

    Added TypeScript module declaration for .png files

---
+++ src/assets/decs.d.ts
@@ -0,0 +1,3 @@
+// In order to import an image in TypeScript, we need to declare it as a module.
+// Here we declare every file with .png extension as a module so, we could import them in TypeScript.
+declare module "*.png";
