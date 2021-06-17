cfdfe92039469f0eb8dbb264dde5c4cf0903939c
Guy Noimark <TEST>
2021-06-17 14:53:37 +0300

    Added inchToMeter convertion

--- src/utils/Math.ts
+++ src/utils/Math.ts
@@ -2,3 +2,4 @@ const radiansToDegrees: number = 180 / Math.PI;
 const degreesToRadians: number = Math.PI / 180;
 export const degrees = (radians: number): number => radians * radiansToDegrees;
 export const radians = (degrees: number): number => degrees * degreesToRadians;
+export const inchToMeter = (inches: number): number => inches * 0.0254;
