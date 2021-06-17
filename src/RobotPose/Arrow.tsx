a83dfeebeec26674d87139394f51475f9f3da03b
Noam Prag <noam.prag0@gmail.com>
2021-06-17 17:58:54 +0300

    Fixed Arrow to work with Field and Robot Components

--- src/RobotPose/Arrow.tsx
+++ src/RobotPose/Arrow.tsx
@@ -1,38 +1,74 @@
 import React from "react";
 import Vector from "../utils/Vector";
+import { FieldConvertion, FieldData, FieldElement, Pose } from "./Field";
 
-interface ArrowProps {
-  readonly lengthInPixels: number;
-  readonly position?: Vector;
-  readonly rotationInRadians?: number;
-  readonly color?: string;
+export interface ArrowPose extends Pose {
+  readonly vector: Vector;
 }
 
-const Arrow = ({
-  lengthInPixels,
-  position,
-  rotationInRadians = 0,
-  color = "black",
-}: ArrowProps): JSX.Element => (
+export interface ArrowProps {
+  readonly pose?: ArrowPose;
+  readonly color: string;
+}
+
+export const Arrow = (props: ArrowProps): JSX.Element => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 330 56"
     style={{
-      width: lengthInPixels,
-      transform: `rotate(${-rotationInRadians}rad)`,
       transformOrigin: "center left",
-      position: "relative",
-      left: position.x,
-      top: position.y,
+      transform: `rotate(${-props.pose.vector.angle}rad)`,
+      position: "absolute",
+      width: props.pose.vector.norm,
+      left: props.pose.position.x,
+      top: props.pose.position.y - (16.97 / 100 / 2) * props.pose.vector.norm,
     }}
   >
-    <line x1="0" y1="28" x2="250" y2="28" stroke={color} strokeWidth="8" />
+    <line
+      x1="0"
+      y1="28"
+      x2="250"
+      y2="28"
+      stroke={props.color}
+      strokeWidth="8"
+    />
     <polygon
-      transform="translate(250, 0) scale(8)"
-      points="0 0, 10 3.5, 0 7"
-      fill={color}
+      transform="translate(250, 0)"
+      points="0 0, 80 28, 0 56"
+      fill={props.color}
     />
   </svg>
 );
 
-export default Arrow;
+const convertArrowToField: FieldConvertion = (
+  pose: ArrowPose,
+  field: FieldData
+): ArrowPose => {
+  const xRatio: number = field.imgSize.x / field.dimensions.x;
+  const yRatio: number = field.imgSize.y / field.dimensions.y;
+
+  const position: Vector = new Vector(
+    pose.position.x * xRatio,
+    field.imgSize.y - pose.position.y * yRatio
+  );
+
+  const vector: Vector = new Vector(
+    pose.vector.x * xRatio,
+    pose.vector.y * yRatio
+  );
+
+  return {
+    position,
+    vector,
+  };
+};
+
+const fieldArrow = (props: ArrowProps, pose: ArrowPose): FieldElement => {
+  return {
+    pose,
+    jsx: <Arrow {...props} />,
+    convertToField: convertArrowToField,
+  };
+};
+
+export default fieldArrow;
