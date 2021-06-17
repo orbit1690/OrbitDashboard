e9aa2cb0ddd48256583f77293b70eeb4681869af
Noam Prag <noam.prag0@gmail.com>
2021-06-17 19:44:25 +0300

    Added limelight image and module decleration for .jpg

--- src/assets/decs.d.ts
+++ src/assets/decs.d.ts
@@ -1,3 +1,4 @@
 // In order to import an image in TypeScript, we need to declare it as a module.
 // Here we declare every file with .png extension as a module so, we could import them in TypeScript.
 declare module "*.png";
+declare module "*.jpg";
