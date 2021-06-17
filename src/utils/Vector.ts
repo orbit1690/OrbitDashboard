5af1831316ef15ef35a6ad4342aaff2f1489ad66
Noam Prag <noam.prag0@gmail.com>
2021-06-17 17:57:53 +0300

    Added utility functions to Vector

--- src/utils/Vector.ts
+++ src/utils/Vector.ts
@@ -9,6 +9,22 @@ export default class Vector {
     return Math.atan2(this.y, this.x);
   }
 
+  public scale(factor: number): Vector {
+    return new Vector(this.x * factor, this.y * factor);
+  }
+
+  public add(other: Vector): Vector {
+    return new Vector(this.x + other.x, this.y + other.y);
+  }
+
+  public static unit(angle: number): Vector {
+    return new Vector(Math.cos(angle), Math.sin(angle));
+  }
+
+  public static polar(angle: number, norm: number): Vector {
+    return Vector.unit(angle).scale(norm);
+  }
+
   public static get random(): Vector {
     return new Vector(Math.random(), Math.random());
   }
