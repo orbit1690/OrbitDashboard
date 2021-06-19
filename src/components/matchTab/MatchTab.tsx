abc6c3192cad0bae46b9818de7426c97687fb38a
Noam Prag <noam.prag0@gmail.com>
2021-06-19 17:09:58 +0300

    Added components to MatchTab

--- src/components/matchTab/MatchTab.tsx
+++ src/components/matchTab/MatchTab.tsx
@@ -4,10 +4,34 @@ import LimeAndField from "./LimeAndField";
 
 import { Grid } from "@material-ui/core";
 
+import ReadyToShoots from "./ReadyToShoots";
+import ShootingParams from "./ShootingParams";
+import AutoChooser from "./AutoChooser";
+
 const MatchTab = (): JSX.Element => {
   return (
     <Grid container direction="column">
       <LimeAndField />
+
+      <Grid
+        container
+        direction="row-reverse"
+        justify="space-around"
+        spacing={5}
+        style={{ marginTop: 16 }}
+      >
+        <Grid item>
+          <ReadyToShoots limeReady={true} armReady={true} wheelsReady={false} />
+        </Grid>
+
+        <Grid item>
+          <ShootingParams />
+        </Grid>
+
+        <Grid item>
+          <AutoChooser />
+        </Grid>
+      </Grid>
     </Grid>
   );
 };
