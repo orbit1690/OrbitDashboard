ae55aa63cd2f1dec3bc52f991273a6f0af5d5aa5
Noam Prag <noam.prag0@gmail.com>
2021-06-19 15:34:23 +0300

    Added elevation effect to ReadyToShoot

--- src/components/matchTab/ReadyToShoots.tsx
+++ src/components/matchTab/ReadyToShoots.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Grid, Switch, withStyles, Typography } from "@material-ui/core";
+import { Grid, Switch, withStyles, Typography, Paper } from "@material-ui/core";
 import { green } from "@material-ui/core/colors";
 
 const GreenSwitch = withStyles({
@@ -18,40 +18,47 @@ const GreenSwitch = withStyles({
 
 const ReadyToShoots = (): JSX.Element => {
   return (
-    <Grid container direction="column" alignItems="center">
-      <Grid item>
-        <Typography variant="h5">Ready To Shoot:</Typography>
+    <Paper elevation={10} style={{ marginTop: 10 }}>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        style={{ padding: 16 }}
+      >
+        <Grid item>
+          <Typography variant="h5">Ready To Shoot:</Typography>
+        </Grid>
+        <Grid item>
+          <Grid
+            container
+            direction="row"
+            alignItems="center"
+            justify="space-between"
+          >
+            <Typography>Lime:</Typography>
+            <GreenSwitch />
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            alignItems="center"
+            justify="space-between"
+          >
+            <Typography>Arm:</Typography>
+            <GreenSwitch />
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            alignItems="center"
+            justify="space-between"
+          >
+            <Typography>Wheels:</Typography>
+            <GreenSwitch />
+          </Grid>
+        </Grid>
       </Grid>
-      <Grid item>
-        <Grid
-          container
-          direction="row"
-          alignItems="center"
-          justify="space-between"
-        >
-          <Typography>Lime:</Typography>
-          <GreenSwitch />
-        </Grid>
-        <Grid
-          container
-          direction="row"
-          alignItems="center"
-          justify="space-between"
-        >
-          <Typography>Arm:</Typography>
-          <GreenSwitch />
-        </Grid>
-        <Grid
-          container
-          direction="row"
-          alignItems="center"
-          justify="space-between"
-        >
-          <Typography>Wheels:</Typography>
-          <GreenSwitch />
-        </Grid>
-      </Grid>
-    </Grid>
+    </Paper>
   );
 };
 
