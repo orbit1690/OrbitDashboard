81e4eaeb7e2c51d0c4bb1fe9d18e88db6f5e56e9
Noam Prag <noam.prag0@gmail.com>
2021-06-24 12:16:09 +0300

    Added AutoDrive to layout

--- src/components/matchTab/layout/SecondRow.tsx
+++ src/components/matchTab/layout/SecondRow.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Box, Grid } from "@material-ui/core";
 import Lime from "../cards/Lime";
+import AutoDrive from "../cards/AutoDrive";
 
 const SecondRow = (): JSX.Element => (
   <Box marginLeft={4} marginTop={-3}>
-    <Grid direction="row" item container justify="flex-start">
+    <Grid direction="row" item container justify="flex-start" spacing={2}>
       <Grid item xs={3}>
         <Lime />
+      </Grid>
+      <Grid item xs={2}>
+        <AutoDrive />
       </Grid>
     </Grid>
   </Box>
