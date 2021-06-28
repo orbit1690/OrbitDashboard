5d326529dd933ccadf74e39cf4e4d681004b185d
Noam Prag <noam.prag0@gmail.com>
2021-06-29 00:11:26 +0300

    Ripple color depends on theme

--- src/components/common/ClickableTile.tsx
+++ src/components/common/ClickableTile.tsx
@@ -4,23 +4,19 @@ import { CardHeader } from "@material-ui/core";
 import { TileProps } from "./Tile";
 import { isDarkTheme } from "../../Theme";
 
-const ClickableTile = (props: TileProps): JSX.Element => {
-  const { ripple } = useTheme().palette;
-
-  return (
-    <CardActionArea
-      style={{
-        height: "100%",
-        borderRadius: 15,
-        color: isDarkTheme() ? ripple.dark : ripple.light,
-      }}
-    >
-      <Card variant="outlined" style={{ height: "100%" }}>
-        <CardHeader title={props.title} />
-        {props.children}
-      </Card>
-    </CardActionArea>
-  );
-};
+const ClickableTile = (props: TileProps): JSX.Element => (
+  <CardActionArea
+    style={{
+      height: "100%",
+      borderRadius: 15,
+      color: useTheme().palette.ripple.main,
+    }}
+  >
+    <Card variant="outlined" style={{ height: "100%" }}>
+      <CardHeader title={props.title} />
+      {props.children}
+    </Card>
+  </CardActionArea>
+);
 
 export default ClickableTile;
